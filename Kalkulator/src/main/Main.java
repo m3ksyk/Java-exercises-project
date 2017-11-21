45eba9eafcb6e38194d1895a50e1e46d5bf8b897
m3ksyk <32710982+m3ksyk@users.noreply.github.com>
2017-11-21 17:33:06 +0100

    Initial summary
    Frame for the brewing calculator.
    To do: all propgram operations and functions, comments, notes, error handling.

---
+++ Kalkulator/src/main/Main.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author User
+ */
+public class Main extends javax.swing.JFrame{
+
+    public void initComponents(){
+    
+    }
+    /**
+     * @param args the command line arguments
+     */
+   
+    public static void main(String[] args) {
+
+    
+  
+                new Menu().setVisible(true);
+            
+        // TODO code application logic here
+    }
+    
+}
