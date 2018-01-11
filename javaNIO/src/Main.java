450c3be0c742a3d52f443af52db2033c5e87b65b
m3ksyk <32710982+m3ksyk@users.noreply.github.com>
2018-01-11 10:33:02 +0100

    update

--- javaNIO/src/Main.java
+++ javaNIO/src/Main.java
@@ -1,6 +1,7 @@
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.file.FileSystems;
@@ -16,13 +17,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // NIO binary read write exercise
+        // NIO binary read write exercise (and other)
 
         try(FileOutputStream binFile = new FileOutputStream("data.dat");
             FileChannel binChannel = binFile.getChannel()){
 
             byte[] outputBytes = "Hello World!".getBytes();
-            ByteBuffer buffer = ByteBuffer.wrap(outputBytes);
+            ByteBuffer buffer = ByteBuffer.allocate(outputBytes.length);
+            buffer.put(outputBytes);
+            buffer.flip();
+
             int numBytes = binChannel.write(buffer);
             System.out.println("numBytes written: " + numBytes);
 
@@ -38,6 +42,52 @@ public class Main {
             numBytes = binChannel.write(intBuffer);
             System.out.println("Numbytes written: " + numBytes);
 
+            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
+            FileChannel channel = ra.getChannel();
+            outputBytes[0] = 'a';
+            outputBytes[1] = 'b';
+            buffer.flip();
+            long numBytesRead = channel.read(buffer);
+            if(buffer.hasArray()){
+                System.out.println("byte buffer = " + new String(buffer.array()));
+            }
+            //absolute read
+            intBuffer.flip();
+            numBytesRead = channel.read(intBuffer);
+            System.out.println(intBuffer.getInt(0));
+            intBuffer.flip();
+            numBytesRead = channel.read(intBuffer);
+
+            System.out.println(intBuffer.getInt(0));
+
+
+
+            //relative read
+//            intBuffer.flip();
+//            numBytesRead = channel.read(intBuffer);
+//            intBuffer.flip();
+//            System.out.println(intBuffer.getInt());
+//            intBuffer.flip();
+//            numBytesRead = channel.read(intBuffer);
+//            intBuffer.flip();
+//            System.out.println(intBuffer.getInt());
+
+            channel.close();
+            ra.close();
+
+          //  System.out.println("output bytes = " + new String(outputBytes));
+
+         //   System.out.println("output bytes = " + new String(outputBytes));
+
+//            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
+//            byte[] b = new byte[outputBytes.length];
+//            ra.read(b);
+//            System.out.println(new String(b));
+//
+//            long int1 = ra.readInt();
+//            long int2 = ra.readInt();
+//            System.out.println(int1);
+//            System.out.println(int2);
 
 
             //  // simple javaNIO read/write exercise
@@ -51,6 +101,8 @@ public class Main {
  //         for(String  line : lines) {
  //               System.out.println(lines);
  //           }
+
+
         } catch (IOException e){
             e.printStackTrace();
         }
